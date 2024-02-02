2473fc88c1c9cecc6b0707e5d2f6ac62574a8a44
BrunoSong <brunosong@wjtb.net>
2024-02-02 15:00:30 +0900

    config: 메모리 H2 다중 Datasource 설정

---
+++ src/main/java/com/brunosong/springbootexam/config/DataSourceConfig.java
@@ -0,0 +1,25 @@
+package com.brunosong.springbootexam.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfig {
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.member-datasource")
+    public DataSource memberDatasource() {
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.order-datasource")
+    public DataSource orderDatasource() {
+        return DataSourceBuilder.create().build();
+    }
+
+}
---
+++ src/test/java/com/brunosong/springbootexam/config/DataSourceConfigTest.java
@@ -0,0 +1,41 @@
+package com.brunosong.springbootexam.config;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
+
+import javax.sql.DataSource;
+
+@SpringBootTest
+class DataSourceConfigTest {
+
+    @Autowired
+    Environment env;
+
+    @Autowired
+    @Qualifier("memberDatasource")
+    DataSource memberDatasource;
+
+
+    @Autowired
+    @Qualifier("orderDatasource")
+    HikariDataSource orderDatasource;    //추천하지 않는다고 인텔리제이가 알려주지만 작동은 가능하다.
+
+
+    @Test
+    @DisplayName("h2가 2대가 연결이 되었는지 확인한다.")
+    void h2MultiDatasourceConfigCheck() {
+
+        HikariDataSource hikariMemberDataSource = (HikariDataSource) memberDatasource;
+        Assertions.assertThat(hikariMemberDataSource.getJdbcUrl()).isEqualTo(env.getProperty("spring.member-datasource.jdbc-url"));
+
+        Assertions.assertThat(orderDatasource.getJdbcUrl()).isEqualTo(env.getProperty("spring.order-datasource.jdbc-url"));
+
+    }
+
+}
